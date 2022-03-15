9a67f2b5f4f23f1685b5e77d06992f9a4164b8e8
cxjava <cxjava@gmail.com>
2022-03-16 00:14:11 +0800

    #na [xin] init

---
+++ application/src/main/java/com/example/apidemo/ApidemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.apidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApidemoApplication.class, args);
+    }
+}
---
+++ application/src/test/java/com/example/apidemo/ApidemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidemoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
